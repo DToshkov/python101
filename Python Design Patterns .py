00cd7d2e8bf01de5f01df9b4aca375feeab7e370
DToshkov <109114920+DToshkov@users.noreply.github.com>
2024-03-20 15:27:13 -0500

    Add files via upload

---
+++ Python Design Patterns .py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print('doing some data science')
+
+
+# Some text 
+
+# #  Title
+# ## Smaller Title 
+# 
+# 
+# 
+
+# Defining a variable 
+# 
+
+# In[2]:
+
+
+x = 5
+
+
+# In[3]:
+
+
+print(x)
+
+
+# In[4]:
+
+
+x
+
+
+# # Data Structures
+# 
+
+# ## List
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+fruits = ['orange', 'apple', 'pear', 'banana', 'kiwi', 'apple', 'banana']
+
+
+# ## Tuples
+
+# In[ ]:
+
+
+my_tuple = (1,2,3)
+
+
+# In[ ]:
+
+
+len(my_tuple)
+
+
+# In[ ]:
+
+
+# once a tuple is created it cannot be shortened 
+
+
+# ## Dictionaries
+
+# In[7]:
+
+
+my_dictionary ={
+    "apple" : 'a red fruit',
+    'bear': 'an animal'
+}
+
+
+# In[8]:
+
+
+my_dictionary["apple"]
+
+
+# In[ ]:
+
+
+
+
